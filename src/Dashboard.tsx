bc74b3cfc27bed16d85ea2a8c24da8474225e010
Catalin Gabriel Diaconu <catalin.diaconu_ext@sonepar.com>
2023-10-28 12:02:33 +0300

    create react typescript

---
+++ src/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+export const Dashboard: React.FC<Props> = () => {
+    return (
+        <div>
+            {/* Your code here */}
+        </div>
+    );
+};
+
+export default Dashboard;
